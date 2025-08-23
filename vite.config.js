a71530ce4cade4f1075273f14228dd5fbd0962fe
Jin0811 <15797954826@163.com>
2025-08-23 09:24:37 +0800

    feat: 删除vue.config.js文件；修改vite.config.js文件中的base属性

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+
+  // 生产环境使用仓库名，开发环境使用根路径
+  base: process.env.NODE_ENV === "production" ? "/GitHubActionsDemo/" : "/",
+});
